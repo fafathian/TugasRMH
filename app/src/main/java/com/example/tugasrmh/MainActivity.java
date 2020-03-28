2465a2b3546902f7af45fca2f8d8d5a908ef6e15
fafathian <zulfa.fathian.ft18@mail.umy.ac.id>
2020-03-28 09:31:18 +0700

    penambahan fungsi button kembali dan mengedit tampilan

--- app/src/main/java/com/example/tugasrmh/MainActivity.java
+++ app/src/main/java/com/example/tugasrmh/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.tugasrmh;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button kembali = (Button) findViewById(R.id.bttnkmbl);
+        kembali.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+
     }
 }
 
